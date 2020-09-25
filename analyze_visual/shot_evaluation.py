ae7b68d74d548d9797bf90a6738da1705f079728
Theodoros Giannakopoulos <tyiannak@gmail.com>
2020-09-25 16:07:39 +0300

    refactoring

--- analyze_visual/shot_evaluation.py
+++ analyze_visual/shot_evaluation.py
@@ -1,27 +1,27 @@
-from analyze_visual import *
-import statistics
+from analyze_visual import process_video
+import numpy as np
+import os
+import sys
+import glob
 
-def read_file(process_mode,file,moviefile,shots):
-	'''
+def read_file(file, moviefile, shots):
+	"""
 	Read the file and return informations about lists of shots
-	'''
-	lines = []
+	"""
 
+	lines = []
 
 	filename1 = os.path.basename(moviefile)  
 	filename2 = os.path.basename(file) 
-	name1 = filename1.rsplit('.',1)[0]
-	name2 = filename2.rsplit('.',1)[0]
+	name1 = filename1.rsplit('.', 1)[0]
+	name2 = filename2.rsplit('.', 1)[0]
 
 	if name1 == name2:
 		with open(file) as w:
 			lines = w.read().splitlines() 
 
-		y= len(lines)
-		x= len(shots)
-
-		print("------ \nThe number of shots founded: ",x-1)
-		print("The actual number of shots: ",y-1)
+		y = len(lines)
+		x = len(shots)
 
 
 		for i in range(0, len(lines)): 
@@ -31,7 +31,7 @@ def read_file(process_mode,file,moviefile,shots):
 
 
 def calc(annotated_shots,shot_change_times):
-	tolerance, correct_shot,correct_shot_h = 0.5, 0,0
+	tolerance, correct_shot, correct_shot_h = 0.5, 0, 0
 
 	correct_recall, correct_precision = 0, 0
 	for t in annotated_shots:
@@ -42,7 +42,8 @@ def calc(annotated_shots,shot_change_times):
 		if np.abs(t - np.array(annotated_shots)).min() < tolerance:
 			correct_precision += 1
 
-
+	print(correct_recall)
+	print(correct_precision)
 
 	precision= correct_precision/(len(shot_change_times))
 	recall = correct_recall/(len(annotated_shots))
@@ -51,28 +52,26 @@ def calc(annotated_shots,shot_change_times):
 	print ("Precision: {0:.0f}%".format(precision*100))
 	print ("Recall: {0:.0f}%".format(recall*100))	
 
-	return precision,recall
-def single_acc(process_mode,video_path,txt_path,shot_change_times):
+	return precision, recall
 
 
-	annotated_shots =read_file(process_mode,txt_path,video_path,shot_change_times)
+def single_acc(video_path,txt_path,shot_change_times):
+	annotated_shots =read_file(txt_path, video_path, shot_change_times)
 	
 	for i in range(0, len(shot_change_times)): 
 		shot_change_times[i] = float(shot_change_times[i])
 	
 	print("Timestamps for predicted shots: \n",shot_change_times)
 	print("Timestamps for actual shots: \n",annotated_shots)
-
 	calc(annotated_shots,shot_change_times)
 
-def dir_acc(video_path,process_mode,txt_path):
 
-	shot_change_t = []
+def dir_acc(video_path,process_mode,txt_path):
 	types = ('*.avi', '*.mpeg', '*.mpg', '*.mp4', '*.mkv')
 	shot_types = ('*.txt')
 	video_files_list = []
 	shot_list = []
-	prec_list,rec_list=[],[]
+	prec_list, rec_list=[], []
 
 	for files in types:
 		video_files_list.extend(glob.glob(os.path.join(video_path, files)))
@@ -103,17 +102,16 @@ def dir_acc(video_path,process_mode,txt_path):
 
 		shot_change_times.clear()
 
-	#print(statistics.mean(prec_list),statistics.mean(rec_list))
-
 	print ("AVG of Precision: {0:.0f}%".format(statistics.mean(prec_list)*100))
 	print ("AVG of Recall: {0:.0f}%".format(statistics.mean(rec_list)*100))		
         #acc(movieFile,txt_path,process_mode,shot_change_times)
+
 
 def main(argv):
     if len(argv) == 4:
         if argv[1] == "-f":
             _, _, shot_change_t = process_video(argv[2], 2, True, True)
-            single_acc(argv[1],argv[2],argv[3],shot_change_t)
+            single_acc(argv[2], argv[3], shot_change_t)
         elif argv[1] == "-d":  # directory
             dir_name = argv[2]
             dir_acc(dir_name,argv[1],argv[3])

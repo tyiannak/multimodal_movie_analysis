bf8c09252d2514be883c420cc50a0c834fa3595a
apoman38 <apoman38@gmail.com>
2020-11-16 10:44:58 +0200

    add updated aggregate_annotations.py and the output files

--- annotation/aggregate_annotations.py
+++ annotation/aggregate_annotations.py
@@ -10,14 +10,14 @@ def get_stats(file):
     print("\nTotal number of annotations: ",data.shape[0],"\n")
 
     #Number of annotation that every user did
-    print("Annotations of every user:\n",data['Username'].value_counts())   
-
+    print("Annotations of every user:\n",data[' Username'].value_counts())   
+    
     #get confidence
     conf = (pd.crosstab(data.Sample_name,data.Class))
     res = conf.div(conf.sum(axis=1), axis=0)*100
     res = res.reset_index()
     res.to_csv('detailed_confidence.csv', index=False)
-    
+
     #print results to txt file
     sav=res['Sample_name']
     res=res.drop(['Sample_name'],axis=1)
@@ -31,9 +31,9 @@ def get_stats(file):
 
     for index, row in res.iterrows():
         
-        print('The video with name',row['Sample_name']," belongs ",row['max_value'], "to class",row['Max'] )
+        print('The video with name',row['Sample_name']," belongs ",row['max_value'], "% to class",row['Max'] )
         
     sys.stdout = orig_stdout
     f.close()
-    
-get_stats('annotations_database_V2.txt')
+  
+get_stats('annotations_database.txt')

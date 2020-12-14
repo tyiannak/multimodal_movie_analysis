f144c5a3fbb7dc2517ac71b217080e2275c9a7b4
Theodoros Giannakopoulos <tyiannak@gmail.com>
2020-12-14 11:29:50 +0200

    code cleanup. argparse added. verbose.

--- classification/create_dataset.py
+++ classification/create_dataset.py
@@ -1,31 +1,39 @@
 import sys
-sys.path.append('../')
-from annotation.aggregate_annotations import aggregate_annotations,save_to_csv
+import argparse
 import os.path
 import shutil
+sys.path.append('../')
+from annotation.aggregate_annotations import aggregate_annotations, save_to_csv
 
 
-def create_file(file):
+def parse_arguments():
+    """Parse arguments for real time demo.
+    """
+    parser = argparse.ArgumentParser(description="Create Shot "
+                                                 "Classification Dataset")
+    parser.add_argument("-a", "--annotation_file", required=True, nargs=None,
+                        help="Annotation database file")
+    parser.add_argument("-v", "--videos_path", required=True, nargs=None,
+                        help="Videos folder path")
 
-    df = aggregate_annotations(file)
-    
-    ann_gr_2 = df[(df['Number_annotations'] >= 2)
-                    & (df['Confidence'] == 100)]
+    return parser.parse_args()
 
-    save_to_csv(ann_gr_2,'annot_with_conf.csv')
+
+def read_annotation_data_and_aggregate(file_path):
+    df = aggregate_annotations(file_path)
+    ann_gr_2 = df[(df['Number_annotations'] >= 3)
+                  & (df['Confidence'] > 50)]
+    save_to_csv(ann_gr_2, 'temp.csv')
 
     return ann_gr_2
 
 
 def create_dataset(df, path_of_shots, final_path):
 
-    classes = ['Static','Vertical_movement','Titl','Panoramic',
-                'Panoramic_lateral','Panoramic_360','Travelling_in',
-                'Travelling_out','Zoom_in','Zoom_out','Vertigo',
-                'Aerial','Handheld','Car_front_windshield','Car_side_mirror',
-                'None']
-    
-    #Create folders of classes
+    # get list of all possible classes:
+    classes = (df['Winner_annotation'].unique())
+
+    # Create folders of classes
     if os.path.exists('dataset'):
         shutil.rmtree('dataset')
     os.mkdir('dataset')
@@ -34,35 +42,48 @@ def create_dataset(df, path_of_shots, final_path):
     for _class_ in classes:
         os.mkdir(os.path.join('dataset', _class_))
 
-
-    print('Moving shots to folders..')
-    #Move shots to class folder
-    for _class_ in classes:
-
-        df2 = df[(df['Winner_annotation'] == _class_)]    
-
+    print('Copying shots to folders..')
+    count = 0
+    count_moved = 0
+    list_of_videos_in_folder = os.listdir(path_of_shots)
+    for _class_ in classes:  # for each shot class:
+        print(' - - - - - - - - - - - - - - - - -')
+        # get the filenames of the videos belonging to the class:
+        df2 = df[(df['Winner_annotation'] == _class_)]
         names = df2['Sample_Name']
         names = names.values.tolist()
         names = set(names)
-
-        for filename in os.listdir(path_of_shots):
-            if filename in names:        
+        print("Class: " + _class_)
+        print(f"Files annotated to this class: {len(names)}")
+        count_moved_class = 0
+        count += len(names)
+        for f in names:
+            if f in list_of_videos_in_folder:
                 try: 
-                    shutil.copy2(os.path.join(path_of_shots,filename),
-                                    os.path.join(final_path,_class_))
+                    shutil.copy2(os.path.join(path_of_shots, f),
+                                 os.path.join(final_path, _class_))
+                    count_moved_class += 1
+                    count_moved += 1
                 except:
                     print('File not found')
+            else:
+                print(f + " not found!")
+        print(f"Files found and copied: {count_moved_class}")
+
+    print(f"Total files annotated {count}. "
+          f"Total files found and moved {count_moved}")
     print('End of process!')
 
 
-if __name__ == "__main__":   
-    
-    file = '../annotation/annotations_database.txt'
-    aggr_file = create_file(file)
+if __name__ == "__main__":
+    args = parse_arguments()
+    data_base_file_name = args.annotation_file
+    videos_path = args.videos_path
 
-    #Ask user to enter the path
-    user_input = input("Enter the path of video shots: ")
-    assert os.path.exists(user_input), "Path doesn't exist, "+str(user_input)
+    aggr_file = read_annotation_data_and_aggregate(data_base_file_name)
+
+    assert os.path.exists(videos_path), "Video Path doesn't exist, " + \
+                                        str(videos_path)
     
-    create_dataset(aggr_file,user_input,'dataset')
+    create_dataset(aggr_file, videos_path, 'dataset')
 

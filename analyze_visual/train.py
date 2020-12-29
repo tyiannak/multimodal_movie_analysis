fa9335761362335784e22f21230af5cf732c1eab
Theodoros Giannakopoulos <tyiannak@gmail.com>
2020-12-30 01:22:25 +0200

    bug fix in train.py when loading feture files

--- analyze_visual/train.py
+++ analyze_visual/train.py
@@ -11,19 +11,14 @@ import os
 import numpy as np
 import sys
 import fnmatch
-from numpy import mean, std
-from scipy import stats
-from pathlib import Path
 from sklearn import model_selection, preprocessing
-from sklearn.model_selection import cross_validate,cross_val_score
-from sklearn.model_selection import KFold
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from analyze_visual import dir_process_video
-from sklearn.metrics import make_scorer, accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import make_scorer, accuracy_score, precision_score, \
+    recall_score, f1_score
 
 
 def parse_arguments():
@@ -31,12 +26,13 @@ def parse_arguments():
     """
     parser = argparse.ArgumentParser(description="Create Shot "
                                                  "Classification Dataset")
-    parser.add_argument("-v", "--videos_path", required=True, action='append', nargs='+',
-                        help="Videos folder path")
-    parser.add_argument("-a", "--training_algorithms",required=True, action='append', nargs='+',
-                        help="Training Algorithms")
+    parser.add_argument("-v", "--videos_path", required=True, action='append',
+                        nargs='+', help="Videos folder path")
+    parser.add_argument("-a", "--training_algorithms", required=True,
+                        action='append', nargs='+', help="Training Algorithms")
 
     return parser.parse_args()
+
 
 def feature_extraction(videos_path):
     """
@@ -54,11 +50,13 @@ def feature_extraction(videos_path):
         # get list of np files in that folder (where features can have
         # been saved):
         np_feature_files = fnmatch.filter(os.listdir(folder), '*_features.npy')
+        print(np_feature_files)
         # if feature npy files exist:
         if len(np_feature_files) > 0:
             for file in np_feature_files:
-                if os.path.isfile(os.path.join(folder,file)):
-                    x["x_{0}".format(folder)] = np.load(file)
+                if os.path.isfile(os.path.join(folder, file)):
+                    x["x_{0}".format(folder)] = np.load(os.path.join(folder,
+                                                                     file))
         else:
             # calculate features for current folder:
             x["x_{0}".format(folder)],\
@@ -67,35 +65,34 @@ def feature_extraction(videos_path):
     
     return x, name_of_files
 
+
 def data_preparation(x):
     """
     Prepare the data before the training process
     :param x: exracted features from videos
     :return: features and labels
     """
-    x_all = np.empty((0,244),float)
-    y=[]
+    x_all = np.empty((0, 244), float)
+    y = []
 
     for key, value in x.items():
-        
         x_all = np.append(x_all,value,axis=0)
-        
         for i in range(value.shape[0]):
             y.append(str(key))
     
-    #Standarization
+    # Standarization
     scaler = StandardScaler()
     # fit and transform the data
     x_all = scaler.fit_transform(x_all)
 
-    #Encode target labels with value between 0 and n_classes-1
+    # Encode target labels with value between 0 and n_classes-1
     lb = preprocessing.LabelEncoder()
     y = lb.fit_transform(y)
     
     return x_all,y
 
 
-def train_models(x,training_algorithms):
+def train_models(x, training_algorithms):
     """
     Train the given algorithms and print accuracy,precision,recall
     :param x: features
@@ -104,30 +101,25 @@ def train_models(x,training_algorithms):
 
     x_all, y = data_preparation(x)
 
-    scoring = ['precision_macro','recall_macro','accuracy']
+    scoring = ['precision_macro', 'recall_macro', 'accuracy']
     models = []
 
     for algorithm in training_algorithms:
-        
         if algorithm == 'SVM':
-
             models.append(('SVM', SVC()))
             
         elif algorithm == 'Decision_Trees':
-            
             models.append(('CART', DecisionTreeClassifier()))
             
         else:
- 
             models.append(('KNN', KNeighborsClassifier()))
 
-    
-    scoring = {'accuracy' : make_scorer(accuracy_score), 
-           'precision' : make_scorer(precision_score, average = 'macro'),
-           'recall' : make_scorer(recall_score, average = 'macro'), 
-           'f1_score' : make_scorer(f1_score, average = 'macro')}
+    scoring = {'accuracy': make_scorer(accuracy_score),
+               'precision': make_scorer(precision_score, average='macro'),
+               'recall': make_scorer(recall_score, average='macro'),
+               'f1_score': make_scorer(f1_score, average='macro')}
 
-    results=[]
+    results = []
     names = []
 
     for name,model in models:
@@ -135,11 +127,9 @@ def train_models(x,training_algorithms):
         #Slit data to train/test
         kfold = model_selection.KFold(n_splits=10, random_state=1, shuffle=True)
     
-        scores = model_selection.cross_validate(model,
-                                X = x_all,
-                                y = y.ravel(),
-                                scoring=scoring,
-                                cv=kfold, n_jobs=-1)
+        scores = model_selection.cross_validate(model, X=x_all, y=y.ravel(),
+                                                scoring=scoring,
+                                                cv=kfold, n_jobs=-1)
 
         results.append(scores)
         names.append(name)
@@ -149,15 +139,17 @@ def train_models(x,training_algorithms):
             print(msg)
         print('---------')            
 
+
 if __name__ == "__main__":
 
     args = parse_arguments()
     videos_path = args.videos_path
     training_algorithms = args.training_algorithms
 
-    #Convert list of lists to a single list
+    # Convert list of lists to a single list
     videos_path = [item for sublist in videos_path for item in sublist]
-    training_algorithms = [item for sublist in training_algorithms for item in sublist]
+    training_algorithms = [item for sublist in training_algorithms
+                           for item in sublist]
 
     for paths in videos_path:
         assert os.path.exists(paths), "Video Path doesn't exist, " + \
@@ -167,12 +159,12 @@ if __name__ == "__main__":
 
     for algorithm in training_algorithms:
         if algorithm not in available_algorithms:
-
-            sys.exit('%s is not available please read the Usage example'%(algorithm))
+            sys.exit('%s is not available please read the '
+                     'Usage example' % (algorithm))
 
     #Extract features of videos
     x, name_of_files = feature_extraction(videos_path)
 
     #Train the models
-    train_models(x,training_algorithms)
+    train_models(x, training_algorithms)
 

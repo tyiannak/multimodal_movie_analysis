cf5eb7b5b79c39478933e0efd8aea13fa1814fc6
apoman38 <apoman38@gmail.com>
2021-01-13 14:14:39 +0200

    add feature selection

--- analyze_visual/train.py
+++ analyze_visual/train.py
@@ -11,12 +11,16 @@ Extratrees, RandomForest
 
 """
 import warnings
+from collections import deque
 import argparse
 import os
 import numpy as np
 import sys
 import fnmatch
 import itertools
+import plotly
+import plotly.graph_objs as go
+from plotly.subplots import make_subplots
 from sklearn import model_selection, preprocessing
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
@@ -29,6 +33,10 @@ from sklearn.metrics import make_scorer, accuracy_score, precision_score, \
     recall_score, f1_score, plot_confusion_matrix, confusion_matrix
 import matplotlib.pyplot as plt
 from sklearn.model_selection import GridSearchCV, train_test_split
+
+
+from sklearn.feature_selection import RFECV
+from sklearn.model_selection import StratifiedKFold
 
 
 def parse_arguments():
@@ -75,6 +83,55 @@ def feature_extraction(videos_path):
 
     return x, name_of_files
 
+def plot_feature_histograms(list_of_feature_mtr, feature_names,
+                            class_names, n_columns=5):
+    '''
+    Plots the histograms of all classes and features for a given
+    classification task.
+    :param list_of_feature_mtr: list of feature matrices
+                                (n_samples x n_features) for each class
+    :param feature_names:       list of feature names
+    :param class_names:         list of class names, for each feature matr
+    '''
+    n_features = len(feature_names)
+    n_bins = 12
+    n_rows = int(n_features / n_columns) + 1
+    figs = plotly.subplots.make_subplots(rows=n_rows, cols=n_columns,
+                                      subplot_titles=feature_names)
+    figs['layout'].update(height=(n_rows * 250))
+    clr = get_color_combinations(len(class_names))
+
+    print(list_of_feature_mtr.ndim)
+
+    for i in range(n_features):
+        # for each feature get its bin range (min:(max-min)/n_bins:max)
+        f = np.vstack([x[:, i:i + 1] for x in list_of_feature_mtr])
+        bins = np.arange(f.min(), f.max(), (f.max() - f.min()) / n_bins)
+        for fi, f in enumerate(list_of_feature_mtr):
+            # load the color for the current class (fi)
+            mark_prop = dict(color=clr[fi], line=dict(color=clr[fi], width=3))
+            # compute the histogram of the current feature (i) and normalize:
+            h, _ = np.histogram(f[:, i], bins=bins)
+            h = h.astype(float) / h.sum()
+            cbins = (bins[0:-1] + bins[1:]) / 2
+            scatter_1 = go.Scatter(x=cbins, y=h, name=class_names[fi],
+                                   marker=mark_prop, showlegend=(i == 0))
+            # (show the legend only on the first line)
+            figs.append_trace(scatter_1, int(i/n_columns)+1, i % n_columns+1)
+    for i in figs['layout']['annotations']:
+        i['font'] = dict(size=10, color='#224488')
+    plotly.offline.plot(figs, filename="report.html", auto_open=True)
+
+def get_color_combinations(n_classes):
+    clr_map = plt.cm.get_cmap('jet')
+    range_cl = range(int(int(255/n_classes)/2), 255, int(255/n_classes))
+    clr = []
+    for i in range(n_classes):
+        clr.append('rgba({},{},{},{})'.format(clr_map(range_cl[i])[0],
+                                              clr_map(range_cl[i])[1],
+                                              clr_map(range_cl[i])[2],
+                                              clr_map(range_cl[i])[3]))
+    return clr
 
 def data_preparation(x):
     """
@@ -89,15 +146,33 @@ def data_preparation(x):
         x_all = np.append(x_all,value,axis=0)
         for i in range(value.shape[0]):
             y.append(str(key))
-    
+
     # Standarization
     scaler = StandardScaler()
     # fit and transform the data
     x_all = scaler.fit_transform(x_all)
-
+    '''
+    #Fail try to plot features histogram
+    feature_names = [i for i in range(244)]
+    plot_feature_histograms(x_all, feature_names, y)
+    '''
     # Encode target labels with value between 0 and n_classes-1
     lb = preprocessing.LabelEncoder()
     y = lb.fit_transform(y)
+
+    #Feature selection
+    rfecv = RFECV(DecisionTreeClassifier(),cv=StratifiedKFold(10), step=1)
+    rfecv = rfecv.fit(x_all, y)
+
+    print('Optimal number of features: {}'.format(rfecv.n_features_))
+
+    plt.figure(figsize=(16, 9))
+    plt.title('Recursive Feature Elimination with Cross-Validation', fontsize=18, fontweight='bold', pad=20)
+    plt.xlabel('Number of features selected', fontsize=14, labelpad=20)
+    plt.ylabel('% Correct Classification', fontsize=14, labelpad=20)
+    plt.plot(range(1, len(rfecv.grid_scores_) + 1), rfecv.grid_scores_, color='#303F9F', linewidth=3)
+
+    plt.show()
 
     return x_all, y
 

9b98df31255d516edcb65a235031f73fea5e54f2
apoman38 <apoman38@gmail.com>
2021-01-15 15:04:31 +0200

    remove unnecessary code from train.py

--- analyze_visual/train.py
+++ analyze_visual/train.py
@@ -91,54 +91,6 @@ def feature_extraction(videos_path):
 
     return x, name_of_files, f_names
 
-def plot_feature_histograms(list_of_feature_mtr, feature_names,
-                            class_names, n_columns=5):
-    '''
-    Plots the histograms of all classes and features for a given
-    classification task.
-    :param list_of_feature_mtr: list of feature matrices
-                                (n_samples x n_features) for each class
-    :param feature_names:       list of feature names
-    :param class_names:         list of class names, for each feature matr
-    '''
-    n_features = len(feature_names)
-    n_bins = 12
-    n_rows = int(n_features / n_columns) + 1
-    figs = plotly.subplots.make_subplots(rows=n_rows, cols=n_columns,
-                                      subplot_titles=feature_names)
-    figs['layout'].update(height=(n_rows * 250))
-    clr = get_color_combinations(len(class_names))
-
-    for i in range(n_features):
-        # for each feature get its bin range (min:(max-min)/n_bins:max)
-        f = np.vstack([x[:, i:i + 1] for x in list_of_feature_mtr])
-        bins = np.arange(f.min(), f.max(), (f.max() - f.min()) / n_bins)
-        for fi, f in enumerate(list_of_feature_mtr):
-            # load the color for the current class (fi)
-            mark_prop = dict(color=clr[fi], line=dict(color=clr[fi], width=3))
-            # compute the histogram of the current feature (i) and normalize:
-            h, _ = np.histogram(f[:, i], bins=bins)
-            h = h.astype(float) / h.sum()
-            cbins = (bins[0:-1] + bins[1:]) / 2
-            scatter_1 = go.Scatter(x=cbins, y=h, name=class_names[fi],
-                                   marker=mark_prop, showlegend=(i == 0))
-            # (show the legend only on the first line)
-            figs.append_trace(scatter_1, int(i/n_columns)+1, i % n_columns+1)
-    for i in figs['layout']['annotations']:
-        i['font'] = dict(size=10, color='#224488')
-    plotly.offline.plot(figs, filename="report.html", auto_open=True)
-
-def get_color_combinations(n_classes):
-    clr_map = plt.cm.get_cmap('jet')
-    range_cl = range(int(int(255/n_classes)/2), 255, int(255/n_classes))
-    clr = []
-    for i in range(n_classes):
-        clr.append('rgba({},{},{},{})'.format(clr_map(range_cl[i])[0],
-                                              clr_map(range_cl[i])[1],
-                                              clr_map(range_cl[i])[2],
-                                              clr_map(range_cl[i])[3]))
-    return clr
-
 def data_preparation(x, fname):
     """
     Prepare the data before the training process
@@ -158,13 +110,12 @@ def data_preparation(x, fname):
     values = fname.values()
     value_iterator = iter(values)
     fnames = next(value_iterator)
-    
+
     # Standarization
     scaler = StandardScaler()
     
     # fit and transform the data
-    x_all = scaler.fit_transform(x_all)
-        
+    x_all = scaler.fit_transform(x_all)       
     
     # Encode target labels with value between 0 and n_classes-1
     lb = preprocessing.LabelEncoder()

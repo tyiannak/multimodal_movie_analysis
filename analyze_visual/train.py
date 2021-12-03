27d083351852289f6fbbde1da0ebcaa7e2391dd1
apetrogianni <apetrogianni@uth.gr>
2021-12-03 02:02:44 +0200

    pipeline

--- analyze_visual/train.py
+++ analyze_visual/train.py
@@ -9,7 +9,6 @@ Available algorithms for traning: SVM, Decision_Trees, KNN, Adaboost,
 Extratrees, RandomForest
 """
 
-
 import warnings
 import argparse
 import os
@@ -19,9 +18,14 @@ import fnmatch
 import itertools
 from pickle import dump
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import precision_recall_fscore_support
+from imblearn.over_sampling import SMOTE
+from imblearn.pipeline import Pipeline
+from sklearn.model_selection import RepeatedStratifiedKFold
 from sklearn.ensemble import AdaBoostClassifier, ExtraTreesClassifier, \
     RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, \
@@ -30,7 +34,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import GridSearchCV, train_test_split
 import json
 sys.path.insert(0, '..')
-from analyze_visual.analyze_visual import dir_process_video
+from analyze_visual.analyze_visual import *
 
 
 def parse_arguments():
@@ -62,8 +66,10 @@ def feature_extraction(videos_path):
         # been saved):
         np_feature_files = fnmatch.filter(os.listdir(folder), '*_features.npy')
         np_fnames_files = fnmatch.filter(os.listdir(folder), '*_f_names.npy')
+
         print(np_feature_files)
         print(np_fnames_files)
+
         # if feature npy files exist:
         if len(np_feature_files) > 0 and len(np_fnames_files) > 0:
             for file in np_feature_files:
@@ -101,15 +107,17 @@ def data_preparation(x):
         count_classes += 1
         for i in range(value.shape[0]):
             y.append(str(key))
+
     # Convert format of labels
     for i, label in enumerate(y):
         splitting = label.split('/')
         label = splitting[-1]
         y[i] = label
+
     return x_all, y
 
 
-def plot_confusion_matrix(name, cm, classes):
+def plot_confusion_matrix(name, cm, classes, id):
     """
     Plot confusion matrix
     :name: name of classifier
@@ -134,87 +142,86 @@ def plot_confusion_matrix(name, cm, classes):
     plt.tight_layout()
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
-    plt.savefig("shot_classifier_conf_mat_" + str(name) + ".jpg")
+    if id == 0:
+        #plt.savefig(len(videos_path) + "_shot_classifier_conf_mat_" + str(name) + ".jpg")
+        plt.savefig(str(len(videos_path)) + '_shot_classifier_conf_mat_' + str(name) + '.eps', format='eps')
+    elif id == 1:
+        plt.savefig("shot_classifier_conf_mat_window_" + str(name) + ".jpg")
 
 
-def Grid_Search_Process(classifier, grid_param, x_all, y):
+def grid_search_process(classifier, grid_param, x_all, y):
     """
     Hyperparameter tuning process and fit the model
+    (for aggregated features).
     :classifier: classifier for train
     :grid_param: different parameters of classifier to test
     :x_all: features
     :y: labels
     """
 
-    X_train, X_test, y_train, y_test = train_test_split(x_all, y,
-                                                        test_size=0.33)
-    print(X_train.shape, X_test.shape)
-    from imblearn.over_sampling import SMOTE
     smt = SMOTE(random_state=0)
-    X_train, y_train = smt.fit_resample(X_train, y_train)
-    print(X_train.shape, X_test.shape)
-
-    # Define scaler
     scaler = MinMaxScaler()
+    #scaler = StandardScaler()
 
-    # Fit scaler on the training dataset
-    scaler.fit(X_train)
+    # create pipeline
+    pipe = Pipeline(steps=[('smote', smt), ('scaler', scaler), ('clf', classifier)])
 
-    # Transform both datasets
-    X_train_scaled = scaler.transform(X_train)
-    X_test_scaled = scaler.transform(X_test)
+    splits = 5
+    repeats = 10
+    rskf = RepeatedStratifiedKFold(n_splits=splits, n_repeats=repeats)
 
-    gd_sr = GridSearchCV(estimator=classifier,
-                         param_grid=grid_param,
-                         scoring='f1_macro',
-                         cv=5, n_jobs=-1)
+    gd_sr = GridSearchCV(pipe, param_grid=grid_param, scoring='f1_macro',
+                         cv=rskf, n_jobs=-1)
 
-    gd_sr.fit(X_train_scaled, y_train)
-   
-    # Save model    
-    dump(gd_sr, open('shot_classifier_' + str(algorithm)+'.pkl', 'wb'))
+    y_test_aggr = []
+    y_pred_aggr = []
 
-    # Save the scaler
-    dump(scaler, open('shot_classifier_' + str(algorithm) +
-                      '_scaler.pkl', 'wb'))
+    print("\nRepeated Stratified KFold (for aggregated features) has started! Please wait... \n")
+    for train, test in gd_sr.cv.split(x_all, y):
+        X_train, X_test = x_all[train], x_all[test]
+        X_train = np.asmatrix(X_train)
+        X_test = np.asmatrix(X_test)
 
+        y_train = []
+        y_test = []
 
-    class_labels = list(set(y_test))
-    # Plot confusion matrix process
-    y_pred = gd_sr.best_estimator_.predict(X_test_scaled)
-    conf_mat = confusion_matrix(y_test, y_pred, labels=class_labels)
-    print(conf_mat)
+        for i in train:
+            y_train.append(y[i])
+
+        for j in test:
+            y_test.append(y[j])
+
+        gd_sr.fit(X_train, y_train)
+        class_labels = list(set(y_test))
+
+        y_pred = gd_sr.best_estimator_.predict(X_test)
+
+        y_test_aggr.append(y_test)
+        y_pred_aggr.append(y_pred)
+
+    # calculate aggregated metrics process
+    y_test_aggr_flattened = np.concatenate(y_test_aggr).ravel()
+    y_pred_aggr_flattened = np.concatenate(y_pred_aggr).ravel()
+
+    cm = confusion_matrix(y_test_aggr_flattened, y_pred_aggr_flattened)
+    f1_score_macro = f1_score(y_test_aggr_flattened, y_pred_aggr_flattened, average='macro')
+    acc = accuracy_score(y_test_aggr_flattened, y_pred_aggr_flattened)
+    precision_recall_fscore = precision_recall_fscore_support(y_test_aggr_flattened, y_pred_aggr_flattened, average='macro')
+
+    print("\nClassification Report:\n"
+          "accuracy: {:0.2f}%,".format(acc * 100),
+          "precision: {:0.2f}%,".format(precision_recall_fscore[0] * 100),
+          "recall: {:0.2f}%,".format(precision_recall_fscore[1] * 100),
+          "f1_score (macro): {:0.2f}%".format(f1_score_macro * 100))
+    print("\nConfusion matrix\n", cm)
+
+    agg_id = 0
     np.set_printoptions(precision=2)
+    plot_confusion_matrix(str(classifier), cm, classes=class_labels, id=agg_id)
 
-    plot_confusion_matrix(str(classifier), conf_mat, classes=class_labels)
- 
-    return y_test, y_pred   
-    
-
-def save_results(algorithm, y_test, y_pred):
-    """
-    Print the results to files based on classifier  
-    :param algorithm: name of the train algorithm
-    :y_test: values for test
-    :y_pred: predicted values
-    """
-    results = {}
-    class_names = list(set(y_test))
-    precisions = precision_score(y_test, y_pred, average=None,
-                                 labels=class_names)
-    recalls = recall_score(y_test, y_pred, average=None,
-                              labels=class_names)
-
-    results['accuracy'] = accuracy_score(y_test, y_pred)
-    results['f1'] = str(f1_score(y_test, y_pred, average='macro'))
-    results['precision_mean'] = precision_score(y_test, y_pred, average='macro')
-    results['recall_mean'] = recall_score(y_test, y_pred, average='macro')
-    results['precisions'] = {class_names[i]: precisions[i]
-                             for i in range(len(class_names))}
-    results['recalls'] = {class_names[i]: recalls[i]
-                          for i in range(len(class_names))}
-    with open("shot_classifier_" + algorithm + "_results.json", 'w') as fp:
-        json.dump(results, fp, indent=4)
+    #num_of_classes = len(videos_path)
+    #np.save(str(num_of_classes) + "_class_y_test.npy", y_test_aggr_flattened)
+    #np.save(str(num_of_classes) + "_class_y_pred.npy", y_pred_aggr_flattened)
 
 
 def train_models(x, training_algorithms):
@@ -230,39 +237,39 @@ def train_models(x, training_algorithms):
 
     for algorithm in training_algorithms:
         if algorithm == 'SVM':
-            classifier = SVC(probability=True)
+            classifier = SVC()
             grid_param = {
-              'C': [0.1, 0.5, 1, 2, 5, 10, 100],
-              'kernel': ['rbf']}
+              'clf__C': [0.1, 0.5, 1, 2, 5, 10, 100],
+              'clf__kernel': ['rbf']}
         elif algorithm == 'Decision_Trees':
             classifier = DecisionTreeClassifier()
             grid_param = {
-                'criterion': ['gini', 'entropy'],
-                'max_depth': range(1, 10)}
+                'clf__criterion': ['gini', 'entropy'],
+                'clf__max_depth': range(1, 10)}
         elif algorithm == 'KNN':
             classifier = KNeighborsClassifier()
             grid_param = {
-                'n_neighbors': [3, 5, 7],
-                'weights': ['uniform','distance']}
+                'clf__n_neighbors': [3, 5, 7],
+                'clf__weights': ['uniform','distance']}
 
         elif algorithm == 'Adaboost':
             classifier = AdaBoostClassifier()
             grid_param = {
-                 'n_estimators': np.arange(100, 250, 50),
-                 'learning_rate': [0.01, 0.05, 0.1, 1]}
+                 'clf__n_estimators': np.arange(100, 250, 50),
+                 'clf__learning_rate': [0.01, 0.05, 0.1, 1]}
         elif algorithm == 'Extratrees':
             classifier = ExtraTreesClassifier()
             grid_param = {
-                'n_estimators': range(25, 126, 25),
-                'max_features': range(25, 401, 25)}
+                'clf__n_estimators': range(25, 126, 25),
+                'clf__max_features': range(25, 401, 25)}
         else:
             classifier = RandomForestClassifier()
             grid_param = {
-            'n_estimators': [100, 300],
-            'criterion': ['gini', 'entropy']}
+            'clf__n_estimators': [100, 300],
+            'clf__criterion': ['gini', 'entropy']}
 
-        y_test,y_pred = Grid_Search_Process(classifier, grid_param, x_all, y)
-        save_results(algorithm, y_test, y_pred)
+        # Grid Search Process for aggregated features
+        grid_search_process(classifier, grid_param, x_all, y)
 
 
 if __name__ == "__main__":
@@ -271,7 +278,7 @@ if __name__ == "__main__":
     args = parse_arguments()
     videos_path = args.videos_path
     training_algorithms = args.training_algorithms
-   
+
     # Convert list of lists to a single list
     videos_path = [item for sublist in videos_path for item in sublist]
     training_algorithms = [item for sublist in training_algorithms
@@ -279,7 +286,7 @@ if __name__ == "__main__":
 
     for paths in videos_path:
         assert os.path.exists(paths), "Video Path doesn't exist, " + \
-                                    str(paths)
+                                      str(paths)
 
     available_algorithms = ['SVM', 'Decision_Trees', 'KNN', 'Adaboost',
                             'Extratrees', 'RandomForest']
